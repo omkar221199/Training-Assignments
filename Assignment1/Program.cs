48f539a6fefa833f80e7c142c018920a440d9301
Omkar.Gorhe <Omkar.Gorhe@PC-14>
2022-07-16 21:10:39 +0530

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string str = null;
+            try
+            {
+                int output1 = int.Parse(str);
+                Console.WriteLine(output1);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            //-----------------------------------------------------------------------------------------------------
+
+            int output2 = Convert.ToInt32(str);
+            Console.WriteLine($"The output is: {output2}");
+
+            //-----------------------------------------------------------------------------------------------------
+
+            string input = "101ab";
+            int num = 0;
+            bool success = int.TryParse(input, out num);
+            if (success)
+            {
+                Console.WriteLine(num);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect number...");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
