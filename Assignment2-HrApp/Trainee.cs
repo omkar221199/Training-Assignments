804d126c923808ba0c85ef0d9a800bac5c008184
Omkar Gorhe <53113471+omkar221199@users.noreply.github.com>
2022-07-19 02:07:07 +0530

    Create Trainee.cs

---
+++ Assignment2-HrApp/Trainee.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLib
+{
+    public class Trainee: Employee
+    {
+        private int noOfDays;
+        public int NoOfDays
+        {
+            get { return noOfDays; }
+            set { noOfDays = value; }
+        }
+
+        private double ratePerDay;
+        public double RatePerDay
+        {
+            get { return ratePerDay; }
+            set { ratePerDay = value; }
+        }
+
+        public Trainee()
+        {
+
+        }
+
+        public Trainee(string name, string address, int noOfDays, double ratePerDay): base(name, address)
+        {
+            NoOfDays = noOfDays;
+            RatePerDay = ratePerDay;
+        }
+
+        public override double CalculateSalary()
+        {
+            return NoOfDays * RatePerDay;   
+        }
+    }
+}
