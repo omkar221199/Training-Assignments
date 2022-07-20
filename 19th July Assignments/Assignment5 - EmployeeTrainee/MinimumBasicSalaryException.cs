e2f081ca356cf110c5cf332786751749535e1033
Omkar Gorhe <53113471+omkar221199@users.noreply.github.com>
2022-07-20 14:47:18 +0530

    Create MinimumBasicSalaryException.cs

---
+++ 19th July Assignments/Assignment5 - EmployeeTrainee/MinimumBasicSalaryException.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLib
+{
+    public class MinimumBasicSalaryException : Exception
+    {
+        public MinimumBasicSalaryException(string message) : base(message)
+        {
+
+        }
+
+    }
+}
