d910da8781e8d12fe64ca516baaec1a12aba6de4
Omkar Gorhe <53113471+omkar221199@users.noreply.github.com>
2022-07-20 14:46:18 +0530

    Create Trainee.cs

---
+++ 19th July Assignments/Assignment5 - EmployeeTrainee/Trainee.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLib
+{
+    public class Trainee: Employee
+    {
+        private int noOfDays;
+        public int NoOfDays
+        {
+            get { return noOfDays; }
+            set { noOfDays = value; }
+        }
+
+        private double ratePerDay;
+        public double RatePerDay
+        {
+            get { return ratePerDay; }
+            set { ratePerDay = value; }
+        }
+
+        public Trainee()
+        {
+
+        }
+
+        public Trainee(int traineeNo, string name, string address, int noOfDays, double ratePerDay): base(traineeNo, name, address)
+        {
+            NoOfDays = noOfDays;
+            RatePerDay = ratePerDay;
+        }
+
+        public override double CalculateSalary()
+        {
+            return NoOfDays * RatePerDay;   
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\nNo of Days is {NoOfDays}\nRate per Day is {RatePerDay}\nNet Salary is {CalculateSalary()}";
+        }
+    }
+}
