c8803a22d82f7ddef84e88d3290ac625c6973612
Omkar Gorhe <53113471+omkar221199@users.noreply.github.com>
2022-08-08 09:34:37 +0530

    Create Transactions.cs

---
+++ ATM Software App/ATMLib/Transactions.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMLib
+{
+    public class Transactions
+    { 
+
+        public string TranDate { get; set; }
+
+        public int Amount { get; set; }
+
+        public int Type { get; set; }
+    }
+}
